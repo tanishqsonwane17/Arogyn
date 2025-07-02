1b4f15f61c16da38cfd94fb79178e3308fd0dc42
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2025-07-02 22:33:04 +0530

    feat: Add Post and User models with validation schemas

---
+++ Backend/models/usersModel.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: [true, "This email is already taken."],
+      trim: true,
+      minlength: [5, "Email must be atleast 6 characters long."],
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required."],
+      select: false, //This is a security measure to ensure that the password is not sent to the client.
+      trim: true,
+      minlength: [6, "Password must be atleast 6 characters long."],
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationCode: {
+      type: String,
+      select: true,
+    },
+    verficationCodeValidation: {
+      type: Number,
+      select: true,
+    },
+    forgotPasswordCode: {
+      type: String,
+      select: true, // Corrected the typo from 'Select' to 'select'
+    },
+    forgotPasswordCodeValidation: {
+      type: Number,
+      select: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
